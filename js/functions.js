ad35838af283e00769b3b97bbd5f307cfd1a3bce
ksino <30484235+ksino@users.noreply.github.com>
2021-05-16 07:01:06 +0800

    网页显示的文字 通过读取文本和json文件

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,269 @@
-var $window=$(window),gardenCtx,gardenCanvas,$garden,garden;var clientWidth=$(window).width();var clientHeight=$(window).height();$(function(){$loveHeart=$("#loveHeart");var a=$loveHeart.width()/2;var b=$loveHeart.height()/2-55;$garden=$("#garden");gardenCanvas=$garden[0];gardenCanvas.width=$("#loveHeart").width();gardenCanvas.height=$("#loveHeart").height();gardenCtx=gardenCanvas.getContext("2d");gardenCtx.globalCompositeOperation="lighter";garden=new Garden(gardenCtx,gardenCanvas);$("#content").css("width",$loveHeart.width()+$("#code").width());$("#content").css("height",Math.max($loveHeart.height(),$("#code").height()));$("#content").css("margin-top",Math.max(($window.height()-$("#content").height())/2,10));$("#content").css("margin-left",Math.max(($window.width()-$("#content").width())/2,10));setInterval(function(){garden.render()},Garden.options.growSpeed)});$(window).resize(function(){var b=$(window).width();var a=$(window).height();if(b!=clientWidth&&a!=clientHeight){location.replace(location)}});function getHeartPoint(c){var b=c/Math.PI;var a=19.5*(16*Math.pow(Math.sin(b),3));var d=-20*(13*Math.cos(b)-5*Math.cos(2*b)-2*Math.cos(3*b)-Math.cos(4*b));return new Array(offsetX+a,offsetY+d)}function startHeartAnimation(){var c=50;var d=10;var b=new Array();var a=setInterval(function(){var h=getHeartPoint(d);var e=true;for(var f=0;f<b.length;f++){var g=b[f];var j=Math.sqrt(Math.pow(g[0]-h[0],2)+Math.pow(g[1]-h[1],2));if(j<Garden.options.bloomRadius.max*1.3){e=false;break}}if(e){b.push(h);garden.createRandomBloom(h[0],h[1])}if(d>=30){clearInterval(a);showMessages()}else{d+=0.2}},c)}(function(a){a.fn.typewriter=function(){this.each(function(){var d=a(this),c=d.html(),b=0;d.html("");var e=setInterval(function(){var f=c.substr(b,1);if(f=="<"){b=c.indexOf(">",b)+1}else{b++}d.html(c.substring(0,b)+(b&1?"_":""));if(b>=c.length){clearInterval(e)}},75)});return this}})(jQuery);function timeElapse(c){var e=Date();var f=(Date.parse(e)-Date.parse(c))/1000;var g=Math.floor(f/(3600*24));f=f%(3600*24);var b=Math.floor(f/3600);if(b<10){b="0"+b}f=f%3600;var d=Math.floor(f/60);if(d<10){d="0"+d}f=f%60;if(f<10){f="0"+f}var a='<span class="digit">'+g+'</span> days <span class="digit">'+b+'</span> hours <span class="digit">'+d+'</span> minutes <span class="digit">'+f+"</span> seconds";$("#elapseClock").html(a)}function showMessages(){adjustWordsPosition();$("#messages").fadeIn(5000,function(){showLoveU()})}function adjustWordsPosition(){$("#words").css("position","absolute");$("#words").css("top",$("#garden").position().top+195);$("#words").css("left",$("#garden").position().left+70)}function adjustCodePosition(){$("#code").css("margin-top",($("#garden").height()-$("#code").height())/2)}function showLoveU(){$("#loveu").fadeIn(3000)};
+// variables
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden
+let info
+let content
+var clientWidth = $(window).width()
+var clientHeight = $(window).height()
+
+function log() {
+    console.log.apply(console, arguments)
+}
+
+$(function () {
+    // setup garden
+    $loveHeart = $("#loveHeart")
+    var offsetX = $loveHeart.width() / 2
+    var offsetY = $loveHeart.height() / 2 - 55
+    $garden = $("#garden")
+    gardenCanvas = $garden[0]
+    gardenCanvas.width = $("#loveHeart").width()
+    gardenCanvas.height = $("#loveHeart").height()
+    gardenCtx = gardenCanvas.getContext("2d");
+    gardenCtx.globalCompositeOperation = "lighter"
+    garden = new Garden(gardenCtx, gardenCanvas)
+
+    let content = $("#content")
+    let width = $loveHeart.width() + $("#code").width()
+    content.css("width", width)
+    content.css("height", Math.max($loveHeart.height(), $("#code").height()))
+    content.css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10))
+    content.css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10))
+
+    // renderLoop
+    // 渲染爱心图形
+    setInterval(function () {
+        garden.render();
+    }, garden.options.growSpeed)
+})
+
+/**
+ * 改变窗口大小重新加载
+ */
+$(window).resize(function () {
+    var width = $(window).width()
+    var height = $(window).height()
+    log("window", width, height)
+    if (width != clientWidth && height != clientHeight) {
+    //     location.replace(location)
+        $("#code").css("width", width)
+        $("#code").css("height", height)
+
+    }
+})
+
+function getHeartPoint(angle) {
+    var t = angle / Math.PI;
+    var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
+    var y = -20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+    return new Array(offsetX + x, offsetY + y);
+}
+
+/**
+ * 开始绘制心型图形
+ */
+function startHeartAnimation() {
+    var interval = 50
+    var angle = 10
+    var heart = new Array()
+    var animationTimer = setInterval(function () {
+        var bloom = getHeartPoint(angle)
+        var draw = true
+        for (var i = 0; i < heart.length; i++) {
+            var p = heart[i]
+            var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2))
+            if (distance < garden.options.bloomRadius.max * 1.3) {
+                draw = false
+                break
+            }
+        }
+        if (draw) {
+            heart.push(bloom)
+            garden.createRandomBloom(bloom[0], bloom[1])
+        }
+        if (angle >= 30) {
+            clearInterval(animationTimer)
+            show_message()
+        } else {
+            angle += 0.2
+        }
+    }, interval)
+}
+
+function convertText(array) {
+    // array是数组 按照回车符号'\r\n' 切割的文本
+    length = array.length
+    let html = ""
+    for (let i = 0; i < length; i++) {
+        let current = array[i]
+
+        html += `<span class="comments">${current}</span><br/><br/>`
+    }
+    return html
+}
+
+/**
+ * 逐个文字显示
+ */
+(function ($) {
+    $.fn.typewriter = function () {
+        this.each(function () {
+            let $ele = $(this)
+            let str = $ele.html()
+            let progress = 0
+            let index = 0
+
+            $ele.html('')
+            // 修改定时器的间隔时间
+            // read_letter()
+            // log("content", content)
+            // log("str", str)
+            let length = content.length
+            // let t = 50
+            // let textTimer = setInterval(showText, t)
+            // function showText() {
+            //     // test是数组 按照回车符号'\r\n' 切割的文本
+            //     let current = content[index]
+            //     let interval
+            //     if (index < length) {
+            //         let html = `${$ele.html()}<span class="comments">${current}</span><br/><br/>`
+            //         $ele.html(html)
+            //         interval = content[index].length * t
+            //         index++
+            //         clearInterval(textTimer)
+            //     }
+            //     if (index < length) {
+            //         log("interval", interval)
+            //         textTimer = setInterval(showText, interval)
+            //     }
+            // }
+
+            // let waitTimer = setInterval(function () {
+            //     let current = info[index]
+            //     if (current) {
+            //         let html = `${$ele.html()}<span class="comments">${current}</span><br/>`
+            //         $ele.html(html)
+            //         index++
+            //
+            //     } else {
+            //         clearInterval(waitTimer)
+            //     }
+            // }, 75)
+            str = convertText(content)
+            let timer = setInterval(function () {
+                let current = str.substr(progress, 1)
+                // log("current", current)
+
+                if (current === '<') {
+                    progress = str.indexOf('>', progress) + 1
+                } else {
+                    progress++
+                }
+                // 不明白
+                let text = str.substring(0, progress) + (progress & 1 ? '_' : '')
+                // log("text", text)
+                $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''))
+                if (progress >= str.length) {
+                    clearInterval(timer)
+                }
+            }, 75)
+        })
+        return this
+    }
+})(jQuery)
+
+/**
+ * 显示相识的时间
+ */
+function timeElapse(date) {
+    var current = Date()
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000
+    var days = Math.floor(seconds / (3600 * 24))
+    seconds = seconds % (3600 * 24)
+    var hours = Math.floor(seconds / 3600)
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+    seconds = seconds % 3600
+    var minutes = Math.floor(seconds / 60)
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    seconds = seconds % 60
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    let result = `<span class="digit">${days} 天 </span><span class="digit">${hours}  小时 </span><span class="digit">${minutes} 分 </span><span class="digit">${seconds} 秒</span>`
+    //var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"
+    $("#elapseClock").html(result)
+}
+
+/**
+ * 显示心型图的文字
+ */
+function show_message() {
+    adjustWordsPosition()
+    let message = $("#messages")
+    let html = `${info.message}
+                    <div id="elapseClock"></div>`
+    message.html(html)
+    message.fadeIn(5000, function () {
+        showLoveU()
+    })
+}
+
+function adjustWordsPosition() {
+    $('#words').css("position", "absolute")
+    $('#words').css("top", $("#garden").position().top + 195)
+    $('#words').css("left", $("#garden").position().left + 70)
+}
+
+function adjustCodePosition() {
+    $('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2)
+}
+
+/**
+ * 显示心型图形里面的署名
+ */
+function showLoveU() {
+    let love = $('#loveu')
+    let html = `${info.love}<br/><div class="signature"> ${info.signal}</div>`
+    love.html(html)
+    love.fadeIn(3000)
+}
+
+/**
+ * 读取json文件
+ */
+function get_info() {
+    let request = {
+        url: "./data/info.json",
+        type: "get",
+        dataType: "json",
+        async: false,
+        success: read_letter,
+        error: () => {
+            log("fail")
+        }
+    }
+
+    $.ajax(request)
+}
+
+
+function read_letter(data) {
+    info = data
+    let request = {
+        url: `./data/${info.file}`,
+        type: "get",
+        async: false,
+        success: (res) => {
+            content = res.split('\r\n')
+            log(content)
+        },
+        error: () => {
+            log("fail")
+        }
+    }
+
+    $.ajax(request)
+}
