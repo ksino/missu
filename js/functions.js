be212d127509c22d19623e6f8d2d905c6b53f28b
ksino <flycat86@163.com>
2021-05-17 09:24:42 +0800

    debug 4 version 0.0.5

--- js/functions.js
+++ js/functions.js
@@ -61,16 +61,15 @@ function resize_heart() {
     let height = window.screen.availHeight
     log("window", width, height)
     let heart = $("#loveHeart")
-    heart.width
-    heart.css("width", width * 0.9)
-    heart.css("height", width * 0.9 * 625 / 670)
-    $("#code").css("width", width * 2)
-    $("#code").css("height", height)
-    if ($("#code").width() < 900) {
-        $("#code").css("font-size", "2.5em")
-        $("#code").css("margin", "auto")
-    }
-    log("heart", heart.width(), heart.height())
+
+    // heart.css("width", width * 0.9)
+    // heart.css("height", width * 0.9 * 625 / 670)
+    // $("#code").css("width", width * 2)
+    // $("#code").css("height", height)
+    // if ($("#code").width() < 900) {
+    //     $("#code").css("font-size", "2.5em")
+    //     $("#code").css("margin", "auto")
+    // }
 }
 
 function getHeartPoint(angle) {
